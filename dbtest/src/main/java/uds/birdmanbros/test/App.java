400df531e89505018f515a2b7950630f68329fbe
uds vl <uds@vl-Lets>
2017-11-13 18:11:58 +0900

    from the office

---
+++ dbtest/src/main/java/uds/birdmanbros/test/App.java
@@ -0,0 +1,24 @@
+package uds.birdmanbros.test;
+
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.sync.RedisCommands;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    	System.out.format("alo%n");
+    	RedisClient redisClient = RedisClient.create("redis://localhost:6379");
+    	StatefulRedisConnection<String, String> connection = redisClient.connect();
+//    	RedisCommands<String, String> syncCommands = connection.sync();
+//
+//    	syncCommands.set("key", "Hello, Redis!");
+
+    	connection.close();
+    	redisClient.shutdown();
+
+    }
+}
